54a54fc7852b63601a3bef4bc025ec0ee81c429a
ksegorenko <122515534+ksegorenko@users.noreply.github.com>
2023-01-15 13:08:16 +0700

    Update Commands.h

--- libs/Commands.h
+++ libs/Commands.h
@@ -1,83 +1,78 @@
 #pragma once
 
-#include "Galaxy.h"
+#include <memory>
+
 #include "Field.h"
+#include "Galaxy.h"
 #include "Rules.h"
 
 class ICommand {
-public:
-	virtual int execute(Field& field, Galaxy& galaxy, std::vector<std::string> args) = 0;
+ public:
+  virtual int execute(Field& field, Galaxy& galaxy,
+                      std::vector<std::string> args) = 0;
 };
 
-
 class Exit : public ICommand {
-public:
-	int execute(Field& field, Galaxy& galaxy, std::vector<std::string> args) override;
+ public:
+  int execute(Field& field, Galaxy& galaxy,
+              std::vector<std::string> args) override;
 };
 
 class Help : public ICommand {
-public:
-	int execute(Field& field, Galaxy& galaxy, std::vector<std::string> args) override;
+ public:
+  int execute(Field& field, Galaxy& galaxy,
+              std::vector<std::string> args) override;
 };
 
 class Tick : public ICommand {
-public:
-	int execute(Field& field, Galaxy& galaxy, std::vector<std::string> args) override;
-private:
-	void outputField(Field& field);
+ public:
+  int execute(Field& field, Galaxy& galaxy,
+              std::vector<std::string> args) override;
+
+ private:
+  void outputField(Field& field);
 };
 
 class Play : public ICommand {
-public:
-	int execute(Field& field, Galaxy& galaxy, std::vector<std::string> args) override;
-private:
-	void outputField(Field& field);
+ public:
+  int execute(Field& field, Galaxy& galaxy,
+              std::vector<std::string> args) override;
+
+ private:
+  void outputField(Field& field);
 };
 
 class Dump : public ICommand {
-public:
-	int execute(Field& field, Galaxy& galaxy, std::vector<std::string> args) override;
-private:
-	void outputField(std::ofstream&, Field&);
+ public:
+  int execute(Field& field, Galaxy& galaxy,
+              std::vector<std::string> args) override;
+
+ private:
+  void outputField(std::ofstream&, Field&);
 };
 
 class ICommandCreator {
-public:
-	virtual ICommand* create() = 0;
+ public:
+  virtual ICommand* create() = 0;
 };
 
 class ExitCreator : public ICommandCreator {
-	ICommand* create() override {
-		return new Exit;
-	}
+  ICommand* create() override { return new Exit; }
 };
 
 class HelpCreator : public ICommandCreator {
-	ICommand* create() override {
-		return new Help;
-	}
+  ICommand* create() override { return new Help; }
 };
 
 class TickCreator : public ICommandCreator {
-	ICommand* create() override {
-		return new Tick;
-	}
+  ICommand* create() override { return new Tick; }
 };
 
 class PlayCreator : public ICommandCreator {
-	ICommand* create() override {
-		return new Play;
-	}
+  ICommand* create() override { return new Play; }
 };
 
 class DumpCreator : public ICommandCreator {
-	ICommand* create() override {
-		return new Dump;
-	}
+  ICommand* create() override { return new Dump; }
 };
 
-class CommandExceptions : public std::exception {
-public:
-	CommandExceptions(const char* msg) :std::exception(msg) {}
-private:
-};
