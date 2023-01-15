9f15fbf53a948a9cc8754ef34bc0599be42a31c9
ksegorenko <122515534+ksegorenko@users.noreply.github.com>
2023-01-15 13:10:55 +0700

    Update Galaxy.h

--- libs/Galaxy.h
+++ libs/Galaxy.h
@@ -1,22 +1,24 @@
 #pragma once
 #include <fstream>
-#include <vector>
 #include <string>
+#include <vector>
+
 #include "Rules.h"
 
 class Galaxy {
-public:
-	Galaxy() : Size(0), Rules(nullptr) {}
-	std::vector<bool>& GetField();
-	int GetSize();
-	std::string GetName();
-	Rules& GetRules();
-	friend void operator >> (std::ifstream& fin, Galaxy& Galaxy);
-private:
-	std::vector<bool> Field;
-	std::string Name;
-	int Size;
-	Rules* Rules;
+ public:
+  Galaxy() : Size(0), Rule(nullptr) {}
+  std::vector<bool>& GetField();
+  int GetSize();
+  std::string GetName();
+  Rules& GetRules();
+  friend void operator>>(std::ifstream& fin, Galaxy& Galaxy);
+
+ private:
+  std::vector<bool> Field;
+  std::string Name;
+  int Size;
+  Rules* Rule;
 };
 
 void SetGalaxyParameters(std::ifstream&, std::string);
