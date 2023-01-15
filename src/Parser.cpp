ecc7dd098b736fdfec67ba995b8f88a19b30d197
ksegorenko <122515534+ksegorenko@users.noreply.github.com>
2023-01-15 13:17:45 +0700

    Update Parser.cpp

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,13 +1,16 @@
-#include "Parser.h"
+#include "../libs/Parser.h"
+
+#include <fstream>
 
 void Parser::CheckCommandLineArguments(int argc, char* argv[]) {
-	if (argc != 2)
-		throw ParsingExceptions("ERROR. Please, enter the correct number of arguments to the command line.");
-	this->fin.open(argv[1]);
-	if (!this->fin.is_open()) 
-		throw ParsingExceptions("ERROR. Input file can not be opened. Please, try again.");
+  if (argc != 2)
+    throw std::invalid_argument(
+        "ERROR. Please, enter the correct number of arguments to the command "
+        "line.");
+  this->fin.open(argv[1]);
+  if (!this->fin.is_open())
+    throw std::invalid_argument(
+        "ERROR. Input file can not be opened. Please, try again.");
 }
 
-std::ifstream& Parser::GetFin() {
-	return this->fin;
-}
+std::ifstream& Parser::GetFin() { return this->fin; }
